86ff9852d7c2a90ec538fd2a6b85ca55b22c834f
ultramarichka <maryna.sokolova11@gmail.com>
2017-12-22 12:15:57 +0100

    add cheating half circle hovers - attach callbacks to events on it

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 document.body.onload = demo();
 
+//??how to make opacity in linear-gradient if I set color here
 function demo(){
-  new Slider(document.body, 99);
+  new Slider(document.body, 100, 0, 750, 1, '#fbe5f7');
 }
 
-function Slider(parent, R){
+function Slider(container, R, min_value, max_value, step, color){
   var self = this;
   this.r = R*0.8;
-  this.fi = 0; 
+  self.fi0 = -Math.PI/2;
+  self.fi = 0 ; 
+  
   this.dh = (R - this.r) + 8; 
-  this.parent = parent;
+  this.container = container;
   self.beingDragged = false;
 
   var r = this.r;
   var dh = this.dh;
-  var fi = this.fi; 
+  var fi = self.fi;
+  var fi0 = self.fi0; 
 
   this.div_slider = document.createElement("div");
   this.div_slider.id = "slider";
-  this.parent.appendChild(this.div_slider);
+  this.container.appendChild(this.div_slider);
 
   this.div_oCircle = document.createElement("div");
   this.div_oCircle.id = "outer_circle";
@@ -29,22 +33,44 @@ function Slider(parent, R){
   this.div_oCircle.style.opacity = "0.6";
   this.div_slider.appendChild(this.div_oCircle);
 
+  //right half-circle hover
+  this.div_oCircleHover = document.createElement("div");
+  this.div_oCircleHover.style.width=R+"px";
+  this.div_oCircleHover.style.height=2*R+"px";
+  this.div_oCircleHover.style.borderRadius = R+"px 0 0 "+R+"px";
+  this.div_oCircleHover.style.background = "linear-gradient(0deg, rgb(0,255,0,0.5), rgb(0,255,0,0) 100px)";
+  this.div_oCircleHover.style.zIndex = "1";
+  this.div_oCircle.appendChild(this.div_oCircleHover);
+  
+  //left half-circle hover
+  this.div_oCircleHoverLeft = document.createElement("div");
+  this.div_oCircleHoverLeft.style.width=R+"px";
+  this.div_oCircleHoverLeft.style.height=2*R+"px";
+  this.div_oCircleHoverLeft.style.right=-R+"px";
+  this.div_oCircleHoverLeft.style.top=-2*R+"px";
+  this.div_oCircleHoverLeft.style.borderRadius = "0 "+R+"px "+R+"px 0";
+  this.div_oCircleHoverLeft.style.background = "linear-gradient(180deg, rgb(0,255,0,1), rgb(0,255,0,0.5) )";
+  this.div_oCircleHoverLeft.style.position = "relative";
+  this.div_oCircleHoverLeft.style.zIndex = "2";
+  this.div_oCircle.appendChild(this.div_oCircleHoverLeft);
 
+  //mask
   this.div_iCircle = document.createElement("div");
   this.div_iCircle.id = "inner_circle";
   this.div_iCircle.style.width= 2*r+"px";
   this.div_iCircle.style.height= 2*r+"px";
   this.div_iCircle.style.borderRadius = r+"px";
   this.div_iCircle.style.background = "white";
-  this.div_iCircle.style.opacity = "0.6";
   this.div_iCircle.style.position = "relative";
   this.div_iCircle.style.left = (R-r)+"px";
   this.div_iCircle.style.top= (R-r)+"px";
-  // find center of the circle in widow coordinates
+  this.div_iCircle.style.zIndex = "3"; 
+  this.div_oCircleHover.appendChild(this.div_iCircle);
+  // distance to top left corner of div_iCircle from widow origin of coordinates
   // nice approach from here https://stackoverflow.com/a/33347664/8325614
-  this.div_oCircle.appendChild(this.div_iCircle);
   var x0 = self.div_iCircle.getBoundingClientRect().left;
   var y0 = self.div_iCircle.getBoundingClientRect().top;
+ 
 
   this.div_handle = document.createElement("div");
   this.div_handle.id = "handle";
@@ -54,29 +80,37 @@ function Slider(parent, R){
   this.div_handle.style.background = "red";
   this.div_handle.style.border = "1px solid #a8a8a8";
   this.div_handle.style.position = "relative";
+  this.div_handle.style.zIndex = "4"; 
   this.div_iCircle.appendChild(this.div_handle);
 
   
   
   self.update = function(fi){
-    self.fi = fi;
-    self.div_handle.style.left= r + (r+(R-r)/2)*Math.cos(fi) - dh/2 +"px";  //x = r*cos(fi); 
-    self.div_handle.style.top= r + (r+(R-r)/2)*Math.sin(fi) - dh/2 +"px";
+    self.div_handle.style.left= r + (r+(R-r)/2)*Math.cos(fi + fi0) - dh/2 +"px";  //x = r*cos(fi); x-coordinate of the #handle
+    self.div_handle.style.top= r + (r+(R-r)/2)*Math.sin(fi +fi0) - dh/2 +"px";   //y - coordinate of the #handle
   }
-  self.update(0);
+  self.update(5*Math.PI/4);
 
   // -----------CALLBACKS--------------------
   function click(e){
     if (!e){e = window.event;} 
     //mask the inner circle https://stackoverflow.com/a/1369080/8325614
-    if( e.target !== self.div_oCircle) return;
+    if (( e.target !== self.div_oCircle) && (e.target !== self.div_oCircleHover) && (e.target !== self.div_oCircleHoverLeft)) {return;}
     // find mouse coordinates
     var x = e.pageX;
     var y = e.pageY;
     //move handle to the coordinates
     fi = Math.atan2(x - x0 - self.r , (y - y0 - self.r));
-    self.update(-fi+Math.PI/2);
-  }
+    self.update(-(fi + Math.PI));
+    if((fi + Math.PI) < Math.PI){
+      self.div_oCircleHoverLeft.style.background = "linear-gradient(180deg, rgb(0,255,0,1), rgb(0,255,0,0.5) )";
+      self.div_oCircleHover.style.background = "linear-gradient(0deg, rgb(0,255,0,0.5), rgb(0,255,0,0) "+ ((y0 + 2*self.r +(R-r)/2) -y) +"px)";
+    }
+    if (((fi + Math.PI) > Math.PI) && ((fi + Math.PI) < 2*Math.PI)) {
+      self.div_oCircleHover.style.background = "";
+      self.div_oCircleHoverLeft.style.background = "linear-gradient(180deg, rgb(0,255,0,1), rgb(0,255,0,0) "+ y  +"px)";
+    }
+ }
 
   function drag(e){
     if (!e){e = window.event;} 
@@ -84,14 +118,26 @@ function Slider(parent, R){
     // find mouse coordinates
     var x = e.pageX;
     var y = e.pageY;
+   
     //move handle to the coordinates
     fi = Math.atan2(x - x0 - self.r , (y - y0 - self.r));
-    self.update(-fi+Math.PI/2);
+    self.update(-(fi + Math.PI));  
+   
+    if((fi + Math.PI) < Math.PI){
+      self.div_oCircleHoverLeft.style.background = "linear-gradient(180deg, rgb(0,255,0,1), rgb(0,255,0,0.5) )";
+      self.div_oCircleHover.style.background = "linear-gradient(0deg, rgb(0,255,0,0.5), rgb(0,255,0,0) "+ ((y0 + 2*self.r +(R-r)/2) -y) +"px)";
+    }
+    if (((fi + Math.PI) > Math.PI) && ((fi + Math.PI) < 2*Math.PI)) {
+      self.div_oCircleHover.style.background = "";
+      self.div_oCircleHoverLeft.style.background = "linear-gradient(180deg, rgb(0,255,0,1), rgb(0,255,0,0) "+ y  +"px)";
+    }
+    
+  
   } 
 
   function enableDrag(e){
     self.beingDragged = true;
-    window.onmousemove = drag;
+    window.onmousemove = drag;  
     drag(e);
   } 
 
@@ -99,13 +145,25 @@ function Slider(parent, R){
     self.beingDragged = false;
     window.onmousemove = undefined;
   }
+  
 
   // -----------ATTACH CALLBACKS------------
   
   this.div_oCircle.onclick = click;
+  this.div_oCircleHover.onclick = click;
+  this.div_oCircleHoverLeft.onclick = click;
   this.div_handle.onmousedown = enableDrag;
 
   window.onmouseup = disableDrag; 
+
+  
+  /*touch events always target the element where that touch STARTED, while mouse events target 
+   the element currently under the mouse cursor.
+
+   -> wait until you get a touchstart event and then add touchmove/touchend/touchcancel handlers
+   to the target of the touchstart event (and remove them on end/cancel)
+   https://www.html5rocks.com/en/mobile/touchandmouse/
+  */
 
 }
 
